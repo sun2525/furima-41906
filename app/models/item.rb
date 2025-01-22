e26a9c1fe8555c002fb75d2771a174ee6ccdbca3
sun2525 <tooooku88@gmail.com>
2025-01-22 18:50:23 +0900

    Revert "必須バリテーション、アソシエーションを記述"
    This reverts commit f38d2e18ba734c059e5a02bac5ae7051e13a4c75.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,3 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
-
-  validates :image, presence: true
-  validates :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
