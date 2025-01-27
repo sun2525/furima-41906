7319224806370d78568bc93eea75af09cfdc3a74
sun2525 <toooku88@gmail.com>
2025-01-27 20:28:05 +0900

    PR後修正2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users # Deviseによるユーザー認証ルート
 
   # アイテム関連のルート
-  resources :items, only: [:new, :create, :index, :show, :edit, :update]
+  resources :items, only: [:new, :create, :index, :show]
 
   # ルートページの設定
   root to: "items#index"
