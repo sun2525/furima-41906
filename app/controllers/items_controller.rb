1d13c3b72b7a0f472bc614cb6b0881b8e933e7ea
sun2525 <toooku88@gmail.com>
2025-01-28 15:09:04 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,55 +1,76 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :redirect_if_not_owner, only: [:edit, :update]
+  # ログイン状態でのみアクセス可能なアクションを指定
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+  # 対象の商品を取得するbefore_actionを追加
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  # 出品者以外はアクセスできないようにリダイレクトするbefore_action
+  before_action :redirect_if_not_owner, only: [:edit, :update, :destroy]
+
+  # 商品一覧ページ
   def index
     @items = Item.includes(:user).order(created_at: :desc) # 商品を取得して変数に格納
   end
 
+  # 商品出品ページ
   def new
     @item = Item.new
   end
 
+  # 商品出品処理
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to root_path # 出品成功後、トップページにリダイレクト
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity # 出品失敗時、エラー表示
     end
   end
 
-  def show  
+  # 商品詳細ページ
+  def show
+    # set_item で @item を取得済み
   end
 
-  def edit 
+  # 商品編集ページ
+  def edit
+    # set_item で @item を取得済み
   end
 
+  # 商品更新処理
   def update
+    # set_item で @item を取得済み
     if @item.update(item_params)
-      redirect_to item_path(@item)
+      redirect_to item_path(@item) # 更新成功後、商品詳細ページにリダイレクト
     else
-      render :edit, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity # 更新失敗時、エラー表示
     end
+  end
+
+  # 商品削除処理
+  def destroy
+    @item.destroy # 商品削除
+    redirect_to root_path # 削除後、トップページにリダイレクト
   end
 
   private
 
+  # Strong Parameters - 商品情報を許可する
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
-                                 :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+                                 :shipping_day_id, :price, :image).merge(user_id: current_user.id) # 出品者IDを加える
   end
 
-  # 対象の@itemを取得
+  # 対象の商品を取得
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:id]) # URLのIDから商品を取得
   end
 
   # 出品者以外をトップページにリダイレクト
   def redirect_if_not_owner
-    return if current_user == @item.user
+    return if current_user == @item.user # 出品者が一致していれば処理を進める
 
-    redirect_to root_path
+    redirect_to root_path # 出品者でなければトップページにリダイレクト
   end
 end
